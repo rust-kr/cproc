53ea2378f4fa18447dcb7a26fe15513784009d94
artblnd <junryoungju@gmail.com>
2021-05-14 16:42:11 +0900

    Added cproc entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::ptr::null;
+use std::ffi::CStr;
+use std::env::args;
+
+#[no_mangle]
+extern fn cproc_main(args: i32, argv: &[*const char]) -> i32;
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<CStr> = args().for_each(|a| CStr::from(a)).collect();
+    let args = argv.len();
+
+    cproc_main(args, argv);
 }
